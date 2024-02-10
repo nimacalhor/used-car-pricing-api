5fe915336aedeea38dd83f2966cb8abb4953f910
nima <nimakalhor25@gmail.com>
2024-02-10 14:51:42 +0330

    #

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+      
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
